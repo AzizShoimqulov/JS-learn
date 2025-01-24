38d7b99e389c0f4aabc3568eb9fe28592cc8d811
AzizShoimqulov <azizshoimqulov74@gmail.com>
2025-01-24 21:28:02 +0500

    add

--- 17_dars.js
+++ 17_dars.js
@@ -1,34 +1,5 @@
+let arr=[10,10,10,10,10]
+console.log(arr);
 
 
-
-
-
-
-
-
-
-const laliga = [
-	{
-		product:"Olma",
-		price: 12000,
-        quantity: 10,
-	},
-	{
-		product:"Anor",
-		price: 15000,
-        quantity: 8,
-	},
-	{
-		product:"Tarvuz",
-		price: 7000,
-        quantity: 5,
-	},
-]
-const sorted = laliga.map((a, b) => {
-	if (price*quantity) {
-		return product(a, b)
-	} else {
-		return "Error"
-	}
-})
-console.log(sorted.join);
+  
